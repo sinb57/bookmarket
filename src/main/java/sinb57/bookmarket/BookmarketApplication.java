51378611872b57a70c64189cbccdb610ae4314b0
sinb57 <sinb57@naver.com>
2021-08-05 23:55:53 +0900

    Initial commit

---
+++ src/main/java/sinb57/bookmarket/BookmarketApplication.java
@@ -0,0 +1,13 @@
+package sinb57.bookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sinb57/bookmarket/BookmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package sinb57.bookmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
