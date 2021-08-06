ef23dc50d435b6db7213eb268c739ff8de75f41c
sinb57 <sinb57@naver.com>
2021-08-06 19:23:39 +0900

    FEAT: MemberMapper 생성
    MemberMapper.java
    MemberMapper.xml

---
+++ src/main/java/sinb57/bookmarket/mapper/member/MemberMapper.java
@@ -0,0 +1,9 @@
+package sinb57.bookmarket.mapper.member;
+
+import org.apache.ibatis.annotations.Mapper;
+import sinb57.bookmarket.domain.member.Member;
+
+@Mapper
+public interface MemberMapper {
+
+}
