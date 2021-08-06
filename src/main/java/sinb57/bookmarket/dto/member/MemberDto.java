2cc2e11a8040036ecac2aab012a1de13f227ca7e
sinb57 <sinb57@naver.com>
2021-08-06 19:55:13 +0900

    FEAT: MemberDto 생성

---
+++ src/main/java/sinb57/bookmarket/dto/member/MemberDto.java
@@ -0,0 +1,12 @@
+package sinb57.bookmarket.dto.member;
+
+import lombok.Getter;
+
+@Getter
+public class MemberDto {
+
+    private String email;
+    private String username;
+    private String password;
+
+}
