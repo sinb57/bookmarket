bd0d52460454efc179f5093cda491616019e5718
sinb57 <sinb57@naver.com>
2021-08-10 16:00:55 +0900

    style: 불필요한 import 제거

--- src/test/java/sinb57/bookmarket/mapper/member/MemberMapperTest.java
+++ src/test/java/sinb57/bookmarket/mapper/member/MemberMapperTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import sinb57.bookmarket.domain.member.Member;
 import sinb57.bookmarket.dto.member.MemberDto;
 
 import static org.assertj.core.api.Assertions.*;
