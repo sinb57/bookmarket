11eddbe09b5fd3071246684cf520a1ca4913e881
sinb57 <sinb57@naver.com>
2021-08-10 18:01:38 +0900

    FEAT: CategoryDto Json 가능토록 수정

--- src/main/java/sinb57/bookmarket/dto/book/CategoryDto.java
+++ src/main/java/sinb57/bookmarket/dto/book/CategoryDto.java
@@ -1,5 +1,7 @@
 package sinb57.bookmarket.dto.book;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import org.apache.ibatis.type.Alias;
@@ -11,8 +13,9 @@ public class CategoryDto {
     private Long id;
     private String name;
 
+    @JsonCreator
     @Builder
-    public CategoryDto(Long id, String name) {
+    public CategoryDto(@JsonProperty("id") Long id, String name) {
         this.id = id;
         this.name = name;
     }
